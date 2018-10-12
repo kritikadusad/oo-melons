9e6a033a43f2eb9b242b4f94e2f06930fee653e8
Hackbright Student <no-reply@hackbrightacademy.com>
2018-10-12 16:55:30 -0700

    Part 2 working

--- melons.py
+++ melons.py
@@ -13,13 +13,17 @@ class AbstractMelonOrder():
 
     def get_total(self):
         """calculate price with tax"""
+        if self.species == 'Christmas':
+            base_price = 5 * 1.5
+        else:
+            base_price = 5
 
-        base_price = 5
-        total = (1 + self.tax) * base_price
 
+        total = ((1 + self.tax) * base_price) + self.fee
+       
         return total
 
-    def mark_shipped(self):
+    def set_shipped(self):
         """record when an order has shipped"""
 
         self.shipped = True
@@ -27,16 +31,26 @@ class AbstractMelonOrder():
 
 class DomesticMelonOrder(AbstractMelonOrder):
     """A melon order within the USA."""
-    
+    fee = 0
     tax = 0.08
 
 
 class InternationalMelonOrder(AbstractMelonOrder):
     """An international (non-US) melon order."""
-    
     tax = 0.17
+
+    def set_fee(self):
+        if self.qty < 10:
+            self.fee = 3
+        else:
+            self.fee = 0    
+    
 
     def get_country_code(self):
         """Return the country code."""
 
         return self.country_code
+
+
+
+
